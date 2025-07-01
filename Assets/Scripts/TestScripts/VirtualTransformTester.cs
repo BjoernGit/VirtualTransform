036230954c2da94f856bfdcddb29324dc90870cd
BjoernGit <49546621+BjoernGit@users.noreply.github.com>
2025-07-01 23:23:51 +0200

    Added demo scene

--- Assets/Scripts/TestScripts/VirtualTransformTester.cs
+++ Assets/Scripts/TestScripts/VirtualTransformTester.cs
@@ -12,17 +12,25 @@ public class VirtualTransformTester : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        //creation of  VirtualTransforms on the basis of an actual Transform
         _virtualTransformChild = new VirtualTransform(child);
-        _virtualTransformChild = new VirtualTransform(betweenChild);
-        _virtualTransformChild.Parent = transform;
+        _virtualTransformBetweenChild = new VirtualTransform(betweenChild);
+
+        //parenting to an actual Transform
+        _virtualTransformBetweenChild.Parent = transform;
+        //parenting to a VirtualTransform
+        _virtualTransformChild.Parent = _virtualTransformBetweenChild;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Setting of the VirtualkTransform position to the Transform to actually move it
         child.position = _virtualTransformChild.Position;
         child.rotation = _virtualTransformChild.Rotation;
 
+        //VirtualTransforms only recalculate on request
         _virtualTransformChild.ShowDebugPose();
+        _virtualTransformBetweenChild.ShowDebugPose();
     }
 }
